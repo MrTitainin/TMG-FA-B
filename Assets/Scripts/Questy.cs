e86f5dee76930551fcf88359eb91387769fe48e7
Antek123 <36418388+Antek123@users.noreply.github.com>
2018-02-25 15:23:59 +0100

    Questy
    1.Pierwsze próby z systemem questów
    2.Ściana nad bramą
    3.Naprawiłem drzwi

---
+++ Assets/Scripts/Questy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Questy : MonoBehaviour {
+    public string nazwa;
+    public bool czy_wykonany;
+    public string opis;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
